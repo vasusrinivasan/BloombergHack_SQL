b62d03e37a4a32c898e02425210f9e0c2c135245
Vasu Srinivasan <vasusrinvsn@gmail.com>
2014-10-05 01:30:04 -0700

    fixed 404 error

--- manage.py
+++ manage.py
@@ -1,18 +1,48 @@
-# Set the path
-import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# # Set the path
+# import os, sys
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from flask.ext.script import Manager, Server
-from app import app
+# from flask.ext.script import Manager, Server
+# from app import app
 
-manager = Manager(app)
+# manager = Manager(app)
 
-# Turn on debugger by default and reloader
-manager.add_command("runserver", Server(
-    use_debugger = True,
-    use_reloader = True,
-    host = '0.0.0.0')
-)
+# # Turn on debugger by default and reloader
+# manager.add_command("runserver", Server(
+#     use_debugger = True,
+#     use_reloader = True,
+#     host = '0.0.0.0')
+# )
 
-if __name__ == "__main__":
-    manager.run()
+# if __name__ == "__main__":
+#     manager.run()
+
+import datetime
+from flask import url_for
+from app import db
+
+
+class Post(db.Document):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    title = db.StringField(max_length=255, required=True)
+    slug = db.StringField(max_length=255, required=True)
+    body = db.StringField(required=True)
+    comments = db.ListField(db.EmbeddedDocumentField('Comment'))
+
+    def get_absolute_url(self):
+        return url_for('post', kwargs={"slug": self.slug})
+
+    def __unicode__(self):
+        return self.title
+
+    meta = {
+        'allow_inheritance': True,
+        'indexes': ['-created_at', 'slug'],
+        'ordering': ['-created_at']
+    }
+
+
+class Comment(db.EmbeddedDocument):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    body = db.StringField(verbose_name="Comment", required=True)
+    author = db.StringField(verbose_name="Name", max_length=255, required=True)
